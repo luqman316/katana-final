3d5a708c9c597ab8ca0e5735e2c139b0933ba9b8
luqman316 <luqmanrehan88@gmail.com>
2025-09-17 16:49:04 +0500

    add backend

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["katanaportfolio.s3.eu-north-1.amazonaws.com"],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "up.yimg.com",
+        hostname: "katanaportfolio.s3.eu-north-1.amazonaws.com",
+        pathname: "/projects/**",
       },
     ],
   },
